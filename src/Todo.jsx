70715ae0322c0dcd1a27773deda5d1f179dd80ca
Nika <donchenko.veronika96@gmail.com>
2023-08-01 23:45:22 +0200

    some

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import {FaRegTrashAlt} from 'react-icons/fa'
 
 const style = {
+    row: `flex`,
+   text: `ml-2 cursor-pointer`,
     li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
     liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
-    row: `flex`,
-    text: `ml-2 cursor-pointer`,
     textComplete: `ml-2 cursor-pointer line-through`,
-    button: `cursor-pointer flex items-center`
+    button: `cursor-pointer flex items-center`,
 }
 
 const Todo = ({todo, toggleComplete, deleteTodo}) => {
